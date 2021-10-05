ed533730e1bf20be165125d7ddb4f0f4a1c11dbe
KevinPC98 <kevin.paredescaman20@gmail.com>
2021-10-04 19:54:26 -0500

    Agregando archivos

---
+++ Assets/Scripts/JohnMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohnMovement : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D Rigidbody2D;
+    private float Horizontal;
+    private Animator Animator;
+    private float LastShoot;
+    public int Health = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>();
+        Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxisRaw("Horizontal");
+        if (Horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f,1.0f,1.0f);
+        } else if (Horizontal > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+
+        Animator.SetBool("running",Horizontal != 0.0f);
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Jump();
+        }
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+        }
+
+    }
+
+    private void Jump()
+    {
+        Rigidbody2D.AddForce(Vector2.up*JumpForce);
+    }
+
+    private void Shoot()
+    {
+        Vector3 direction;
+        if (transform.localScale.x == 1.0f)
+        {
+            direction = Vector2.right;
+        } else
+        {
+            direction = Vector2.left;
+        }
+        GameObject bullet =  Instantiate(BulletPrefab, transform.position+direction*0.1f, Quaternion.identity);
+        bullet.GetComponent<BulletScript>().setDirection(direction);
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody2D.velocity = new Vector2(Horizontal*Speed, Rigidbody2D.velocity.y);
+    }
+
+    public void Hit()
+    {
+        Health = Health - 1;
+        if (Health == 0) Destroy(gameObject);
+    }
+}
